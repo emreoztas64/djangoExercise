457837cc1d13d82ae0a6c22beb2f52ca4b7f8383
emreoztas64 <e.oztas00@gmail.com>
2024-08-22 04:11:52 +0300

    task 2

--- hipogram/posts/views.py
+++ hipogram/posts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+
 from .models import Post
 
 
 def post_list_view(request):
     posts = Post.objects.all().order_by("-creation_datetime")
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
     return render(request, "post_list.html", {'posts': posts})
 
 def create_view(request):
