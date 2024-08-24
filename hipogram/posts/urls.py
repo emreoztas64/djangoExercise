9430e793fa2468ca717f92c853a435081c438d88
emreoztas64 <e.oztas00@gmail.com>
2024-08-24 05:26:09 +0300

    generic views

--- hipogram/posts/urls.py
+++ hipogram/posts/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    path("", views.post_list_view, name="list"),
-    path("user/<str:username>/", views.post_list_view, name="user_list"),
-    path("tag/<str:tag>/", views.post_list_view, name="tag_list"),
-    path("create/", views.create_view, name="create"),
-    path("update/<int:id>/", views.update_view, name="update"),
-    path("delete/<int:id>/", views.delete, name="delete"),
+    path("", views.PostListView.as_view(), name="list"),
+    path("user/<str:username>/", views.PostListView.as_view(), name="user_list"),
+    path("tag/<str:tag>/", views.PostListView.as_view(), name="tag_list"),
+    path("create/", views.PostCreateView.as_view(), name="create"),
+    path("<int:pk>/update/", views.PostUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
 ]
--- hipogram/posts/views.py
+++ hipogram/posts/views.py
@@ -1,62 +1,69 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
-from django.core.paginator import Paginator
-from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 from datetime import date
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post, Tag
 
-def post_list_view(request, tag=None, username=None):
-    tags = Tag.objects.filter(post__creation_datetime__date=date.today()).annotate(count=Count("post")).order_by("-count")[:5]
-    if tag:
-        posts = Post.objects.filter(tags__name=tag).order_by("-creation_datetime")
-    elif username:
-        posts = Post.objects.filter(created_by__username=username).order_by("-creation_datetime")
-    else:
-        posts = Post.objects.all().order_by("-creation_datetime")
-    page_number = request.GET.get("page")
-    paginator = Paginator(posts, 5)
-    posts = paginator.get_page(page_number)
-    return render(request, "post_list.html", {'posts': posts, 'tags': tags})
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+    context_object_name = "posts"
+    paginate_by = 5
+
+    def get_queryset(self):
+        tag = self.kwargs.get("tag")
+        username = self.kwargs.get("username")
+        if tag:
+            return Post.objects.filter(tags__name=tag).order_by("-creation_datetime")
+        elif username:
+            return Post.objects.filter(created_by__username=username).order_by("-creation_datetime")
+        else:
+            return Post.objects.all().order_by("-creation_datetime")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.filter(post__creation_datetime__date=date.today()).annotate(count=Count("post")).order_by("-count")[:5]
+        return context
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["image", "text", "tags"]
+    template_name = "create.html"
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
     
-@login_required
-def create_view(request):
-    if request.method == "POST":
-        post = Post(
-        image = request.FILES.get("image"),
-        text = request.POST.get("text"),
-        created_by = request.user,
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ["image", "text", "tags"]
+    template_name = "update.html"
+    success_url = "/"
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(
+            created_by=self.request.user
         )
-        post.save()
-        tags = request.POST.getlist("tags")
-        post.tags.set(tags)
-        return redirect(reverse("posts:list"))
-    else:
-        tags = Tag.objects.all()
-        return render(request, "create.html", {'tags': tags})
 
-@login_required
-def update_view(request, id):
-    post = get_object_or_404(Post, id=id)
-    if request.user != post.created_by:
-        return redirect(reverse("posts:list"))
-    if request.method == "POST":
-        post.text = request.POST.get("text")
-        post.save()
-        tags = request.POST.getlist("tags")
-        post.tags.set(tags)
-        return redirect(reverse("posts:list"))
-    else:
-        tags = Tag.objects.all()
-        return render(request, "update.html", {'post': post, 'tags': tags})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
 
-    
-@login_required
-def delete(request, id):
-    post = get_object_or_404(Post, id=id)
-    if request.user != post.created_by:
-        return redirect(reverse("posts:list"))
-    else:
-        post.delete()
-        return redirect(reverse("posts:list"))
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = "/"
+    template_name = "delete.html"
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(
+            created_by=self.request.user
+        )
